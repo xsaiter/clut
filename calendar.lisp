c468464edb32a1918a86b86cedd9586d4bb3b2cd
xsaiter <xsaiter@mail.ru>
2017-12-12 21:49:56 +0300

    improve

--- calendar.lisp
+++ calendar.lisp
@@ -3,3 +3,9 @@
       (and (zerop (mod year 4))
 	   (not (zerop (mod year 100))))))
 
+(defun days-of-month (m y)
+  (cond
+    ((member m '(jan mar may jul aug oct dec)) 31)
+    ((member m '(apr jun sep nov)) 30)
+    ((member m '(feb)) (if (leap-year-p y) 29 28))))
+
